12e42f7b2ff8519dec2e224cb9282bbce7178a0e
Tyler Salaj <tsalaj@wisc.edu>
2023-11-15 12:02:18 -0600

    Created platform for everything.

--- app/src/main/java/com/cs407/shopnonstoptylersexample/MainActivity.java
+++ app/src/main/java/com/cs407/shopnonstoptylersexample/MainActivity.java
@@ -2,10 +2,14 @@ package com.cs407.shopnonstoptylersexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +19,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button signUp = findViewById(R.id.buttonSignUp);
+
+        TextView homePage = findViewById(R.id.buttonLogin);
+
+        String username = "username";
+
+        SharedPreferences sharedPreferences = getSharedPreferences("com.cs407.shopnonstoptylersexample", Context.MODE_PRIVATE);
+
+        if(sharedPreferences.getString("username", "")!= ""){
+            Intent noteIntent = new Intent(this, ShopNonStopHomePage.class);
+            noteIntent.putExtra("username", sharedPreferences.getString("username", ""));
+            startActivity(noteIntent);
+        }else{
+            setContentView(R.layout.activity_main);
+        }
+
+        homePage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ShopNonStopHomePage.class);
+                startActivity(intent);
+            }
+        });
 
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/cs407/shopnonstoptylersexample/settingsPage.java
+++ app/src/main/java/com/cs407/shopnonstoptylersexample/settingsPage.java
@@ -1,7 +1,10 @@
 package com.cs407.shopnonstoptylersexample;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
