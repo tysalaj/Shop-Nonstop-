79f6cce7abbd0d7adc5c4740bd226ba67408b0f4
Om Moradia <moradiaom1@gmail.com>
2023-12-04 21:59:54 -0600

    added toast for when users tries to add an empty item and for when the item already exists in the list

--- app/src/main/java/com/cs407/shopnonstoptylersexample/ShoppingList.java
+++ app/src/main/java/com/cs407/shopnonstoptylersexample/ShoppingList.java
@@ -11,6 +11,7 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -23,7 +24,7 @@ public class ShoppingList extends AppCompatActivity {
         setContentView(R.layout.shoppinglistpage);
 
         ImageView settings = findViewById(R.id.settingsIcon);
-        ImageView add = findViewById(R.id.add);
+        ImageView addIcon = findViewById(R.id.addIcon);
         SharedPreferences sharedPreferences = getSharedPreferences("com.cs407.shopnonstoptylersexample", MODE_PRIVATE);
         String username = sharedPreferences.getString("username", "");
         String upUsername = username.substring(0, 1).toUpperCase() + username.substring(1);
@@ -40,37 +41,52 @@ public class ShoppingList extends AppCompatActivity {
 
         Context context = getApplicationContext();
         SQLiteDatabase sqLiteDatabase = context.openOrCreateDatabase("items", Context.MODE_PRIVATE, null);
-        DBHelper helper = new DBHelper(sqLiteDatabase);
+        DBHelper dbHelper = new DBHelper(sqLiteDatabase);
 
-        items = helper.readNotes(username);
+        items = dbHelper.readNotes(username);
         ArrayList<String> shoppingItems = new ArrayList<>();
 
         for (Item item : items) {
             shoppingItems.add(item.getItemName());
         }
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, shoppingItems);
+        ArrayAdapter<String> shoppingListAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, shoppingItems);
         ListView shoppingListView = findViewById(R.id.shoppingListViewPopular);
-        shoppingListView.setAdapter(adapter);
+        shoppingListView.setAdapter(shoppingListAdapter);
 
         ListView shoppingListViewPopular = findViewById(R.id.shoppingListView);
         ArrayList<String> shoppingItemsPop = new ArrayList<>();
         ArrayAdapter<String> adapterPop = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, shoppingItemsPop);
         shoppingListViewPopular.setAdapter(adapterPop);
 
-        add.setOnClickListener(new View.OnClickListener() {
+        addIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 EditText item = findViewById(R.id.addItem);
-                String itemName = item.getText().toString();
-                if (item.getText().toString() != "") {
-                    adapter.add(itemName);
-                    helper.saveNotes(username, itemName);
+                String itemName = item.getText().toString().trim();
+                ArrayList<Item> currShoppingItems = dbHelper.readNotes(username);
+                if (!itemName.equals("")) {
+                    for (Item currItem : currShoppingItems) {
+                        if (currItem.getItemName().toLowerCase().equals(itemName.toLowerCase())) {
+                            displayItemExistsToast();
+                            return;
+                        }
+                    }
+                    shoppingListAdapter.add(itemName);
+                    dbHelper.saveNotes(username, itemName);
+                } else {
+                   displayEmptyItemToast();
                 }
             }
         });
     }
 
+    public void displayItemExistsToast() {
+        Toast.makeText(this, "Item is already in your list!", Toast.LENGTH_SHORT).show();
+    }
 
+    public void displayEmptyItemToast() {
+        Toast.makeText(this, "Need to enter an item name!", Toast.LENGTH_SHORT).show();
+    }
 
 }
