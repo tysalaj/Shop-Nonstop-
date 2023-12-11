b636030550291451325615c57829e4a9bdaaa57c
Om Moradia <moradiaom1@gmail.com>
2023-12-11 13:10:06 -0600

    Added a dialog for when user wants to update distance

--- app/src/main/java/com/cs407/shopnonstoptylersexample/SettingsPage.java
+++ app/src/main/java/com/cs407/shopnonstoptylersexample/SettingsPage.java
@@ -1,16 +1,22 @@
 package com.cs407.shopnonstoptylersexample;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -28,8 +34,7 @@ public class SettingsPage extends AppCompatActivity {
         ImageView imageViewUserImage = findViewById(R.id.imageViewUserImage);
         TextView textViewUserName = findViewById(R.id.textViewUserName);
         Button buttonLogout = findViewById(R.id.buttonLogout);
-
-        EditText editTextsetDistance = findViewById(R.id.editTextSetDistance);
+        Button buttonSetDistance = findViewById(R.id.buttonSetDistance);
 
 
 
@@ -54,9 +59,6 @@ public class SettingsPage extends AppCompatActivity {
             public void onClick(View view){
                 Intent intent = new Intent(SettingsPage.this, ShopNonStopHomePage.class);
                 startActivity(intent);
-                String distanceValue = editTextsetDistance.getText().toString();
-                SharedPreferences sharedPreferences = getSharedPreferences("com.cs407.shopnonstoptylersexample", MODE_PRIVATE);
-                sharedPreferences.edit().putString("distance", distanceValue).apply();
             }
         });
 
@@ -71,5 +73,46 @@ public class SettingsPage extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        buttonSetDistance.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(SettingsPage.this);
+                builder.setTitle("Furthest Distance");
+
+                final EditText input = new EditText(SettingsPage.this);
+                input.setInputType(InputType.TYPE_CLASS_NUMBER |
+                        InputType.TYPE_NUMBER_FLAG_DECIMAL);
+                builder.setView(input);
+
+
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String distanceValue = input.getText().toString();
+                        if (distanceValue.trim().equals("")) {
+                            Toast.makeText(SettingsPage.this, "Cannot enter an empty value", Toast.LENGTH_SHORT).show();
+                            return;
+                        } else if (Double.parseDouble(distanceValue) > 10) {
+                            Toast.makeText(SettingsPage.this, "Cannot set a radius greater than 10 miles", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        SharedPreferences sharedPreferences = getSharedPreferences("com.cs407.shopnonstoptylersexample", MODE_PRIVATE);
+                        sharedPreferences.edit().putString("distance", distanceValue).apply();
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                builder.show();
+
+            }
+        });
     }
+
+
 }
