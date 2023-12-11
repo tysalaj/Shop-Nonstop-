8ee4aba7d7c15f6f7fa5add899a259528cc46b71
Om Moradia <moradiaom1@gmail.com>
2023-12-10 22:16:08 -0600

    Added user database to store user's items using Firebase

--- app/src/main/java/com/cs407/shopnonstoptylersexample/ShoppingList.java
+++ app/src/main/java/com/cs407/shopnonstoptylersexample/ShoppingList.java
@@ -3,31 +3,38 @@ package com.cs407.shopnonstoptylersexample;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
 public class ShoppingList extends AppCompatActivity {
-    private ArrayList<Item> items;
+    private FirebaseDatabase database;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.shoppinglistpage);
 
+        database = FirebaseDatabase.getInstance();
         ImageView settings = findViewById(R.id.settingsIcon);
         ImageView homePageIcon = findViewById(R.id.homePage);
         ImageView addIcon = findViewById(R.id.addIcon);
-        SharedPreferences sharedPreferences = getSharedPreferences("com.cs407.shopnonstoptylersexample", MODE_PRIVATE);
-        String username = sharedPreferences.getString("username", "");
 
         settings.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,58 +44,71 @@ public class ShoppingList extends AppCompatActivity {
             }
         });
 
-        homePageIcon.setOnClickListener(new View.OnClickListener(){
+        homePageIcon.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
+            public void onClick(View view) {
                 Intent intent = new Intent(ShoppingList.this, ShopNonStopHomePage.class);
                 startActivity(intent);
             }
         });
 
-        Context context = getApplicationContext();
-        SQLiteDatabase sqLiteDatabase = context.openOrCreateDatabase("items", Context.MODE_PRIVATE, null);
-        DBHelper dbHelper = new DBHelper(sqLiteDatabase);
 
-        items = dbHelper.readNotes(username);
-        ArrayList<String> shoppingItems = new ArrayList<>();
+        String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        Log.i("INFO", uid);
+        DatabaseReference db = database.getReference();
+        DatabaseReference uidRef = db.child("users").child(uid).child("items");
 
-        for (Item item : items) {
-            shoppingItems.add(item.getItemName());
-        }
+        uidRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                ArrayList<String> items = new ArrayList<>();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    items.add(dataSnapshot.getValue(String.class));
+                }
+                ArrayAdapter<String> shoppingListAdapter = new ArrayAdapter<>(ShoppingList.this, android.R.layout.simple_list_item_1, items);
+                ListView shoppingListView = findViewById(R.id.shoppingListView);
+                shoppingListView.setAdapter(shoppingListAdapter);
+            }
 
-        ArrayAdapter<String> shoppingListAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, shoppingItems);
-        ListView shoppingListView = findViewById(R.id.shoppingListView);
-        shoppingListView.setAdapter(shoppingListAdapter);
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
 
         addIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 EditText item = findViewById(R.id.addItem);
                 String itemName = item.getText().toString().trim();
-                ArrayList<Item> currShoppingItems = dbHelper.readNotes(username);
-                if (!itemName.isEmpty()) {
-                    for (Item currItem : currShoppingItems) {
-                        if (currItem.getItemName().toLowerCase().equals(itemName.toLowerCase())) {
-                            displayItemExistsToast();
-                            return;
+                if (itemName.isEmpty()) {
+                    Toast.makeText(ShoppingList.this, "Need to enter an item name", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                uidRef.orderByValue().equalTo(itemName).addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (snapshot.exists()) {
+                            Toast.makeText(ShoppingList.this, "Item already exists!", Toast.LENGTH_SHORT).show();
+                        } else {
+                            String itemId = uidRef.push().getKey();
+                            uidRef.child(itemId).setValue(itemName).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    Toast.makeText(ShoppingList.this, "Added New Item", Toast.LENGTH_SHORT).show();
+                                    item.setText("");
+                                }
+                            });
                         }
                     }
-                    shoppingListAdapter.add(itemName);
-                    dbHelper.saveNotes(username, itemName);
-                    item.setText("");
-                } else {
-                   displayEmptyItemToast();
-                }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
             }
         });
     }
-
-    public void displayItemExistsToast() {
-        Toast.makeText(this, "Item is already in your list!", Toast.LENGTH_SHORT).show();
-    }
-
-    public void displayEmptyItemToast() {
-        Toast.makeText(this, "Need to enter an item name!", Toast.LENGTH_SHORT).show();
-    }
-
 }
