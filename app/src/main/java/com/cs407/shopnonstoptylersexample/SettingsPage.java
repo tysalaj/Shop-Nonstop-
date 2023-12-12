a970896f70ec09d826cbbb3696157f6bb1bfdfae
Eddie Engelman <eddieengelman@gmail.com>
2023-12-11 22:44:55 -0600

    Image Upload Working Version

--- app/src/main/java/com/cs407/shopnonstoptylersexample/SettingsPage.java
+++ app/src/main/java/com/cs407/shopnonstoptylersexample/SettingsPage.java
@@ -1,9 +1,12 @@
 package com.cs407.shopnonstoptylersexample;
 
+import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
@@ -11,6 +14,11 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
@@ -23,10 +31,27 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
 import java.util.Objects;
+import java.io.IOException;
 
 public class SettingsPage extends AppCompatActivity {
     private FirebaseAuth auth;
+
+    Button BSelectImage;
+
+    ImageView IVPreviewImage;
+
+    int SELECT_PICTURE = 200;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +64,18 @@ public class SettingsPage extends AppCompatActivity {
         Button buttonLogout = findViewById(R.id.buttonLogout);
         Button buttonSetDistance = findViewById(R.id.buttonSetDistance);
         Button buttonEditProfile = findViewById(R.id.editProfile);
+        // register the UI widgets with their appropriate IDs
+        BSelectImage = findViewById(R.id.BSelectImage);
+        IVPreviewImage = findViewById(R.id.imageViewUserImage);
+
+        // handle the Choose Image button to trigger
+        // the image chooser function
+        BSelectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageChooser();
+            }
+        });
 
         String email = Objects.requireNonNull(auth.getCurrentUser()).getEmail();
         textViewUserName.setText(email);
@@ -163,6 +200,46 @@ public class SettingsPage extends AppCompatActivity {
             }
         });
     }
+
+
+
+    private void imageChooser()
+    {
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+
+        launchSomeActivity.launch(i);
+    }
+
+    ActivityResultLauncher<Intent> launchSomeActivity
+            = registerForActivityResult(
+            new ActivityResultContracts
+                    .StartActivityForResult(),
+            result -> {
+                if (result.getResultCode()
+                        == Activity.RESULT_OK) {
+                    Intent data = result.getData();
+                    // do your operation from here....
+                    if (data != null
+                            && data.getData() != null) {
+                        Uri selectedImageUri = data.getData();
+                        Bitmap selectedImageBitmap = null;
+                        try {
+                            selectedImageBitmap
+                                    = MediaStore.Images.Media.getBitmap(
+                                    this.getContentResolver(),
+                                    selectedImageUri);
+                        }
+                        catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        IVPreviewImage.setImageBitmap(
+                                selectedImageBitmap);
+                    }
+                }
+            });
+
 
 
 }
